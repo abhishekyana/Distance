579b357ac24869b4758f9f48168eb58b3c526717
Abhishek <abhishekasdzxc@gmail.com>
2017-04-16 23:43:56 +0530

    Update asimo_dist.py

--- asimo_dist.py
+++ asimo_dist.py
@@ -2,24 +2,21 @@ import json, sys, requests
 import os
 
 def distances(place1,place2):
-    origin_city = place1
-    destination_city = place2
+    key= "AIzaSyDlx_bbdCwiAzIsYHLb4ka5ZwlRN8u1Haw"
 
-    api_key = "AIzaSyDlx_bbdCwiAzIsYHLb4ka5ZwlRN8u1Haw"
-
-    send_url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins={0}&destinations={1}&key={2}"
+    url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins={0}&destinations={1}&key={2}"
 
     try:
-        j = json.loads(requests.get(send_url.format(str(origin_city),str(destination_city),str  (api_key))).text)
+        j = json.loads(requests.get(url.format(str(place1),str(place2),str(key))).text)
         if j["status"] == "OK":
             if j["rows"][0]["elements"][0]["status"] == "OK":
-                print(j["rows"][0]["elements"][0]["distance"]["text"]) # + "\n" + j["rows"][0]["elements"][0]["duration"]["text"])
-                os.system('say '+'"'+'the distance is '+str(j["rows"][0]["elements"][0]["distance"]["text"])+'"')
+                print(j["rows"][0]["elements"][0]["distance"]["text"]) + "\n" + j["rows"][0]["elements"][0]["duration"]["text"])
+                #os.system('say '+'"'+'the distance is '+str(j["rows"][0]["elements"][0]["distance"]["text"])+'"') still in beta.
             else:
-                print("Data not found")
-                os.system("say Sorry Couldn't find")
+                print("Something Went Wrong")
+               # os.system("say Sorry Couldn't find")
         else:
             print("Error")
     except BaseException as e:
-        print("Failed to request: " + str(e))
-#distances('visakhapatnam','mumbai')
+        print("Failed the request :" + str(e))
+distances('visakhapatnam','mumbai')
